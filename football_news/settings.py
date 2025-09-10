1eed25c72e368315c5eec45ec30f33902f0e3b30
Jaysen Lestari <lestarijaysen@gmail.com>
2025-09-10 15:24:38 +0700

    Tutor2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -28,7 +28,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","jaysen-lestari-footballnews.pbp.cs.ui.ac.id"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://jaysen-lestari-footballnews.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
